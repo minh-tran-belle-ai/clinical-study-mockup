dc38e076720c3dd148edbea36970a83bd60c37ee
Minh Tran <mtran@belle.ai>
2021-10-08 15:31:31 +0700

    add delete image

--- src/redux/type.d.ts
+++ src/redux/type.d.ts
@@ -263,8 +263,8 @@ export const initialState: AllState = {
             ]
         },
         {
-            "name": "acne",
-            "altName": "make your face acne free",
+            "name": "EASI",
+            "altName": "The Eczema Area and Severity Index (EASI) is a validated scoring system that grades the physical signs of atopic dermatitis/eczema. EASI is the core outcome for measuring the clinical signs of eczema in all trials.",
             "startDate": "10/10/2020",
             "endDate": "11/11/2021",
             "organization": "Roche Holding AG",
@@ -272,28 +272,58 @@ export const initialState: AllState = {
             "progressBar": 50,
             "image": [
                 {
-                    "link": "https://images.medicinenet.com/images/slideshow/boils_s4_cystic_acne.jpg",
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYvZ1mbO_9FHb3lGWRPPlTBdoq9qjslZYbLmI1IpKGApr6dAi9RqR14X65p4VtGKgAzqk&usqp=CAU",
                     "date": "10/10/2020",
                     "group": 1
                 },
                 {
-                    "link": "https://assets.nhs.uk/prod/images/S_0917_acne_M1080444.max-600x600.jpg",
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6_PMH9ybAPDZStrAeofbE46JMnEHaMHDEzlWhZYv-cCuNjr3ymf_i4fuohFfidnVzuG0&usqp=CAU",
                     "date": "10/10/2020",
                     "group": 1
                 },
                 {
-                    "link": "https://cdn.shopify.com/s/files/1/0557/9803/8687/articles/acne.jpg?v=1625317030",
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-bSjkp2soH_hJ7TAC1a8f_3DbD1iRirQAaV8iSH6_lPz0MmgvrWmFcNKFgJkjB7_JDsE&usqp=CAU",
                     "date": "10/10/2020",
                     "group": 2
                 },
                 {
-                    "link": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-851423236-1558637428.jpg?crop=0.713xw:1.00xh;0.216xw,0&resize=480:*",
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ9OJI6kFemDsBy31kFBsjqpTaJtXUybvr5paTcwCSPw7v2FqhLSCNnzaI3TfDxibFKOs&usqp=CAU",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2021/08/the-girl-has-dermatitis-on-the-neck-1296x728-header-1024x575.jpg?w=1155&h=1528",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://www.researchgate.net/profile/Mikiya-Sato/publication/41620575/figure/fig1/AS:202521114091520@1425296204710/Eczema-on-the-patients-neck-photographed-on-day-5.png",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5UVEH5dNbtKRjUtG5tek0nfbCPQWoHVQJo4dPJeQXHoC6K6t62ddxz-iJm-v0AA21RFQ&usqp=CAU",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFbZY_WA4ZeSJTUYmsJ691-fhdF521Is33uluMzvX2c7KUIJj9LLTsZDbhttBtBilXNlw&usqp=CAU",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzGPnuMMtGaYIXWBle8O5XdeQN6IbG9GOvkLn4h90q2Wcz2z5y_AcwwqQYHwpbHaGXTA4&usqp=CAU",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm8PghS8Grt6MJTTC-Kum_-md9ShcrnayITdGYQ8D7RhawZI-Nu_bIgTKk99YC9fjsVvA&usqp=CAU",
                     "date": "10/10/2020",
                     "group": 3
                 }
             ],
             "user": ["Click", "Clint", "Caz"],
-            "scoreType": ["Overall", "Erythema", "Induration", "Desquamation"],
+            "scoreType": ["Overall", "Head and Neck", "Upper Limbs", "Lower Limbs", "Trunk"],
             "status": "ongoing",
             "pastScore": 2,
             "score": [
@@ -320,8 +350,8 @@ export const initialState: AllState = {
             ]
         },
         {
-            "name": "cancer",
-            "altName": "finding the cure to cancer",
+            "name": "SCORAD",
+            "altName": "SCORAD is a clinical tool used to assess the extent and severity of eczema (SCORing Atopic Dermatitis).",
             "startDate": "10/10/2020",
             "endDate": "11/11/2021",
             "organization": "Eli Lily and co",
@@ -329,22 +359,47 @@ export const initialState: AllState = {
             "progressBar": 30,
             "image": [
                 {
-                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpruPcm_caTDI9vlIN4A-VxK6dWb9FarF5ew&usqp=CAU",
+                    "link": "https://images.medicinenet.com/images/image_collection/pediatrics/atopic-dermatitis-8-2.jpg",
                     "date": "10/10/2020",
                     "group": 1
                 },
                 {
-                    "link": "https://images.onhealth.com/images/slideshow/cancer-101-s1-what-is-cancer-cell.jpg",
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5OfrbyqXc9kWdXvydxzxVsmHghOOGycn-XoBrK6tOzPnNpyFW_sJ7pCqopH_Nk5hEp54&usqp=CAU",
                     "date": "10/10/2020",
                     "group": 1
                 },
                 {
-                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsKLxIUlDRtI4MPGwhdIhO6zPdHjM3LxrCxNXKMZY9ub5A-l2O0lrnCjiWa2N-UB992vA&usqp=CAU",
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHmQqw7rwvB8f9gGw_ARO8bxi3xlKbHA9T0QEQhK6Cc2qWLfh1L7bzA5ZX_hIpruEoFuQ&usqp=CAU",
                     "date": "10/10/2020",
                     "group": 2
                 },
                 {
-                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXMRj4pEluPpu9WzyqGLLbENXhikS259IH6tn3OyIKLlsE-f0s2Klj1oMzfiBPK3qJRRI&usqp=CAU",
+                    "link": "https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/06/GettyImages-1138468353_header-1024x575.jpg?w=1155&h=1528",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://images.squarespace-cdn.com/content/v1/5a7b2e05bff2001bc750b07d/1520532268876-85ESK1G817WDO3X29CS9/a+patient+itching+their+eczema-affected+hands",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcGvu6vJbuREAhNKk2q87hk12mSbvuRp4n1JVQ9Vg_Y3FwGeX6NytMGwbD608tTdGzSlw&usqp=CAU",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://images.rxlist.com/images/image_collection/pediatrics/scarlet-fever-3-24.jpg",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://www.todayonline.com/sites/default/files/styles/new_app_article_detail/public/photos/43_images/eczema1.jpg?itok=uZVroJeQ",
+                    "date": "10/10/2020",
+                    "group": 3
+                },
+                {
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYcdHAmUyWT0DAF6eOvdt2dpmTRv5AZ3fhpAComl-xmSgEEloWxhH3u9x0BeqIvYM0ip0&usqp=CAU",
                     "date": "10/10/2020",
                     "group": 3
                 }
@@ -377,8 +432,8 @@ export const initialState: AllState = {
             ]
         },
         {
-            "name": "skin",
-            "altName": "Rid you of skin diseases",
+            "name": "Nail Psoriasis (NAPS)",
+            "altName": "Nail psoriasis alters the way your toenails and fingernails look. They may get thick, develop pinprick holes and change color or shape. ",
             "startDate": "10/10/2020",
             "endDate": "11/11/2021",
             "organization": "Eli Lily and co",
@@ -386,25 +441,40 @@ export const initialState: AllState = {
             "progressBar": 100,
             "image": [
                 {
-                    "link": "https://images-prod.healthline.com/hlcmsresource/images/topic_centers/4468-skin-layers-1296x728-body-50.jpg",
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgt4HasFvtgaRzk4cZFgRMy-UVz9i9-XRlfyHmaqF4HC8EqsDO3DRhv6eQ8Ga5t4rt6gw&usqp=CAU",
                     "date": "10/10/2020",
                     "group": 1
                 },
                 {
-                    "link": "https://previews.123rf.com/images/hfsimaging/hfsimaging1509/hfsimaging150900013/44912780-section-through-skin-showing-normal-epidermis-and-skin-surface-structure-resulting-in-water-loss-and.jpg",
+                    "link": "https://www.researchgate.net/publication/271730201/figure/fig2/AS:398565353508865@1472036793321/Nail-bed-psoriasis-of-fingernails_Q640.jpg",
                     "date": "10/10/2020",
                     "group": 1
                 },
                 {
-                    "link": "https://i0.wp.com/microbeonline.com/wp-content/uploads/2021/06/Skin-microbiome.jpg?fit=1010%2C1050&ssl=1",
+                    "link": "https://media.springernature.com/original/springer-static/image/chp%3A10.1007%2F978-3-319-08810-5_11/MediaObjects/315148_1_En_11_Fig12_HTML.gif",
                     "date": "10/10/2020",
-                    "group": 2
+                    "group": 1
                 },
                 {
-                    "link": "https://www.researchgate.net/publication/337569825/figure/fig1/AS:829932498321413@1574882735330/Schematic-cross-section-of-skin-with-its-appendages-Reprinted-from-3-with-permission.png",
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv3IDFeSRGmLDUYvUogTT5vExe5bUgdi75q6qFMDqONAoTKdQ-YUkWmEK6I39voq5J9sU&usqp=CAU",
                     "date": "10/10/2020",
-                    "group": 3
-                }
+                    "group": 1
+                },
+                {
+                    "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRssCQ68ItmmJfO5dbFvmNnHub0bRI4I0wFw&usqp=CAU",
+                    "date": "10/10/2020",
+                    "group": 1
+                },
+                {
+                    "link": "https://thumbs.dreamstime.com/b/damaged-toenails-onychomycosis-nail-psoriasis-damaged-toenails-onychomycosis-nail-psoriasis-rough-foot-white-background-213809393.jpg",
+                    "date": "10/10/2020",
+                    "group": 1
+                },
+                {
+                    "link": "https://img.etimg.com/thumb/msid-66048660,width-1200,height-900,imgsize-463403,overlay-etpanache/photo.jpg",
+                    "date": "10/10/2020",
+                    "group": 1
+                },
             ],
             "user": ["Nam", "Nait", "Nemur"],
             "scoreType": ["Overall", "Erythema", "Induration", "Desquamation"],
